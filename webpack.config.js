b20d817fc2aae682d416ea96da053cb498f36e53
zhuyingjie <m6567fc@outlook.com>
2020-10-30 16:42:02 +0800

    add webpack

---
+++ webpack.config.js
@@ -0,0 +1,40 @@
+const path = require('path')
+
+module.exports = {
+    // mode: 'development',
+    // devtool: 'inline-source-map',
+    entry: path.join(__dirname, "src/index.ts"),
+    output: {
+        path: path.join(__dirname, "dist"),
+        filename: 'bundle.js',
+        library: 'tdsSDK',
+        libraryTarget: 'umd',        
+    },
+    resolve: {
+        // Add `.ts` and `.tsx` as a resolvable extension.
+        extensions: [".ts", ".tsx", ".js"],
+        // ignore node module 
+        fallback : {
+            'child_process': false,
+            'crypto': false,
+            'buffer': false,
+            'http': false,
+            'url': false
+        }
+    },
+    module: {
+        rules: [
+            // all files with a `.ts` or `.tsx` extension will be handled by `ts-loader`
+            { test: /\.tsx?$/, loader: "ts-loader"},
+            {    
+                test: /\.js$/,    
+                exclude: /node_modules/,    
+                loader: 'babel-loader'    
+            }            
+        ]
+    },
+    externals: {
+        // require('ws') as WebSocket
+        ws: 'WebSocket'
+    }
+}
