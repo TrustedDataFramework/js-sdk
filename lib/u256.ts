c875279741ca6504f647cd5e4e1838e1b9779029
zhuyingjie <m6567fc@outlook.com>
2020-11-24 16:21:08 +0800

    添加u256的除法 tostring

--- lib/u256.ts
+++ lib/u256.ts
@@ -1,8 +1,13 @@
-import { Util } from "./util";
+import { Util } from "./util"
 
-export class U256{
+const chars = '0123456789'
+
+export class U256 {
     private static ONE: U256 | null = null
     public static ZERO: U256 = new U256(new Uint8Array(32).buffer)
+
+    private static MAX_VALUE: U256 | null = null
+
     readonly buf: ArrayBuffer
 
     @operator("+")
@@ -14,33 +19,52 @@ export class U256{
         return left.safeSub(right);
     }
 
+    @operator("*")
+    static __op_mul(left: U256, right: U256): U256 {
+        return left.safeMul(right)
+    }
+
+    // 除法不会溢出，除数不能为 0
+    @operator("/")
+    static __op_div(left: U256, right: U256): U256 {
+        return left.divMod(right)[0]
+    }
+
+    // 求模不会溢出，除数不能为 0
+    @operator("%")
+    static __op_mod(left: U256, right: U256): U256 {
+        return left.divMod(right)[1]
+    }    
+
     @operator(">")
-    static __op_gt(left: U256, right :U256): bool {
+    static __op_gt(left: U256, right: U256): bool {
         return left.compareTo(right) > 0;
     }
     @operator(">=")
-    static __op_gte(left: U256, right :U256): bool {
+    static __op_gte(left: U256, right: U256): bool {
         return left.compareTo(right) >= 0;
     }
     @operator("<")
-    static __op_lt(left: U256, right :U256): bool {
+    static __op_lt(left: U256, right: U256): bool {
         return left.compareTo(right) < 0;
     }
     @operator("<=")
-    static __op_lte(left: U256, right :U256): bool {
+    static __op_lte(left: U256, right: U256): bool {
         return left.compareTo(right) <= 0;
     }
     @operator("==")
-    static __op_eq(left: U256, right :U256): bool {
+    static __op_eq(left: U256, right: U256): bool {
         return left.compareTo(right) == 0;
     }
 
-    compareTo(u: U256): i32{
+
+
+    compareTo(u: U256): i32 {
         return Util.compareBytes(this.buf, u.buf)
     }
 
-    static one(): U256{
-        if(U256.ONE != null)
+    static one(): U256 {
+        if (U256.ONE != null)
             return changetype<U256>(U256.ONE)
 
         const buf = new Uint8Array(32)
@@ -49,8 +73,20 @@ export class U256{
         return changetype<U256>(U256.ONE)
     }
 
-    constructor(buf: ArrayBuffer){
-        if(buf.byteLength > 32){
+    static max(): U256 {
+        if (U256.MAX_VALUE != null)
+            return changetype<U256>(U256.ONE)
+
+        const buf = new Uint8Array(32)
+        for(let i = 0; i < buf.length; i++){
+            buf[i] = u8(0xff)
+        }
+        U256.MAX_VALUE = new U256(buf.buffer)
+        return changetype<U256>(U256.MAX_VALUE)
+    }
+
+    constructor(buf: ArrayBuffer) {
+        if (buf.byteLength > 32) {
             unreachable()
         }
         const u = new Uint8Array(32)
@@ -58,36 +94,81 @@ export class U256{
         this.buf = u.buffer
     }
 
-    add(u: U256): U256{
+    add(u: U256): U256 {
         const l = Uint8Array.wrap(this.buf)
         const r = Uint8Array.wrap(u.buf)
         const newData = new Uint8Array(32)
-        for (let i = 31, overflow = 0; i >= 0; i--) {
-            let v = (l[i] as u32) + (r[i] as u32) + overflow
-            newData[i] = u8(v)
+        for (let i = 31, overflow: u32 = 0; i >= 0; i--) {
+            let v = u32(l[i]) + u32(r[i]) + overflow
+            newData[i] = u8(v & 0xff)
             overflow = v >>> 8
         }
         return new U256(newData.buffer)
     }
 
-    sub(u: U256): U256{
+    sub(u: U256): U256 {
         const r = Uint8Array.wrap(u.buf)
         const neg = new Uint8Array(32)
-        for(let i = 0; i < neg.length; i++){
+        for (let i = 0; i < neg.length; i++) {
             neg[i] = ~r[i]
         }
         const n = new U256(neg.buffer)
         return this.add(n.add(U256.one()))
     }
 
-
-    // 左移动一个 bit ，相当于乘以2
-    private lshift(): U256{
+    // 左移 n 个 单位
+    lshiftN(n: i32): U256 {
+        if (n < 0)
+            unreachable()
+        let count = n / 8
+        let offset = u8(n % 8)
         let r = new Uint8Array(32)
         let u = Uint8Array.wrap(this.buf)
-        for(let i = 0; i < r.length; i++){
-            r[i] = (u[i] << 1) & 0xff
-            if(i + 1 < u.length){
+        for (let i = 31; i >= 0; i--) {
+            let j = i - count
+            if (j >= 0) {
+                // u8 << 8 会被忽略，所以先转成 u32 再移位
+                r[j] |= u8((u32(u[i]) << offset) & u32(0xff))
+            }
+            if (j > 0) {
+                const mask = (u32(0xff) << (8 - offset))
+                const x = (u32(u[i]) & mask) >> (8 - offset)
+                r[j - 1] |= u8(x & 0xff)
+            }
+        }
+        return new U256(r.buffer)
+    }
+
+    rshiftN(n: i32): U256 {
+        if (n < 0)
+            unreachable()
+
+        let count = n / 8
+        let offset = u8(n % 8)
+
+        let r = new Uint8Array(32)
+        let u = Uint8Array.wrap(this.buf)
+        for (let i = 0; i < u.length; i++) {
+            let j = i + count
+            if (j < r.length) {
+                r[j] |= u8((u32(u[i]) >> offset) & 0xff)
+            }
+            if (j + 1 < r.length) {
+                const mask = u32(0xff) >> (8 - offset)
+                let x = (u32(u[i]) & mask) << (8 - offset)
+                r[j + 1] |= u8(x & 0xff)
+            }
+        }
+        return new U256(r.buffer)
+    }
+
+    // 左移动一个 bit ，相当于乘以2
+    lshift(): U256 {
+        let r = new Uint8Array(32)
+        let u = Uint8Array.wrap(this.buf)
+        for (let i = 0; i < r.length; i++) {
+            r[i] = u[i] << 1
+            if (i + 1 < u.length) {
                 r[i] |= (u[i + 1] & 0x80) >> 7
             }
         }
@@ -95,35 +176,45 @@ export class U256{
     }
 
     // 右移动一个 bit ，相当于除以2
-    private rshift(): U256{
+    rshift(): U256 {
         let r = new Uint8Array(32)
         let u = Uint8Array.wrap(this.buf)
-        for(let i = u.length - 1; i >= 0; i--){
-            r[i] = (u[i] >> 1) & 0xff
-            if(i - 1 >= 0){
+        for (let i = u.length - 1; i >= 0; i--) {
+            r[i] = u[i] >> 1
+            if (i - 1 >= 0) {
                 r[i] |= (u[i - 1] & 0x01) << 7
             }
         }
         return new U256(r.buffer)
     }
 
-    lastBit(): bool{
+    lastBit(): bool {
         let arr = Uint8Array.wrap(this.buf)
         return (arr[31] & 1) > 0
     }
 
-    mul(r: U256): U256{
+    mul(r: U256): U256 {
         let ret = U256.ZERO
         let l = new U256(this.buf)
-        while (!r.isZero()){
+        while (!r.isZero()) {
             let n = r.lastBit()
-            if(n){
+            if (n) {
                 ret = ret.add(l)
             }
             l = l.lshift()
             r = r.rshift()
         }
         return ret
+    }
+
+    safeMul(u: U256): U256 {
+        if (this == U256.ZERO || u == U256.ZERO) {
+            return U256.ZERO;
+        }
+
+        const c = this.mul(u)
+        assert(c.div(this).compareTo(u) == 0, "SafeMath: multiplication overflow ")
+        return c
     }
 
     safeAdd(u: U256): U256 {
@@ -138,25 +229,66 @@ export class U256{
         return this.sub(u);
     }
 
-    isZero(): bool{
+    isZero(): bool {
         const u = Uint8Array.wrap(this.buf)
-        for(let i = 0; i < u.length; i++){
-            if(u[i] != 0)
+        for (let i = 0; i < u.length; i++) {
+            if (u[i] != 0)
                 return false
         }
         return true
     }
 
-    toString(): string{
-        return Util.encodeHex(this.buf)
+    toString(): string {
+        let ret = ''
+        const BASE = U256.fromU64(10)
+        let n = new U256(this.buf)
+        if(n == U256.ZERO)
+            return '0'
+        while(n > U256.ZERO){
+            const dm = n.divMod(BASE)
+            n = dm[0]
+            let m = dm[1]
+            ret = chars.charAt(i32(m.u64())) + ret
+        }
+        return ret
     }
 
-    u64(): u64{
+    u64(): u64 {
         const arr = this.buf.slice(24, 32)
         return Util.bytesToU64(arr)
     }
 
-    static fromU64(u: u64): U256{
+    div(divisor: U256): U256{
+        return this.divMod(divisor)[0]
+    }
+
+
+    divMod(divisor: U256): U256[] {
+        if (divisor == U256.ZERO)
+            // 除法溢出
+            unreachable()
+        let dividend = new U256(this.buf)
+        let quo = U256.ZERO
+        for (let i = 255; i >= 0; i--) {
+            //比较dividend是否大于divisor的(1<<i)次方，不要将dividend与(divisor<<i)比较，而是用(dividend>>i)与divisor比较，
+            //效果一样，但是可以避免因(divisor<<i)操作可能导致的溢出，如果溢出则会可能dividend本身小于divisor，但是溢出导致dividend大于divisor       
+            if (dividend.rshiftN(i) >= divisor) {
+                quo = quo.add(U256.oneLshift(i))
+                dividend = dividend.safeSub(divisor.lshiftN(i))
+            }
+        }
+        return [quo, dividend]
+    }
+
+    public static oneLshift(i: i32): U256 {
+        let count = i / 8
+        let offset = i % 8
+        const r = new Uint8Array(32)
+        r[31 - count] = (1 << offset)
+        return new U256(r.buffer)
+    }
+
+    static fromU64(u: u64): U256 {
         const bytes = Util.u64ToBytes(u)
         const v = Uint8Array.wrap(bytes)
         const arr = new Uint8Array(32)
@@ -164,7 +296,7 @@ export class U256{
         return new U256(arr.buffer)
     }
 
-    static fromHex(hex: string): U256{
+    static fromHex(hex: string): U256 {
         const buf = Util.decodeHex(hex)
         return new U256(buf)
     }
--- test/u256.ts
+++ test/u256.ts
@@ -4,7 +4,21 @@ import { U256, log, Util, Context, Address } from '../lib'
 const u = U256.fromHex('ffffffffffffffff')
 
 export function init(): Address{
-    assert(u - U256.one() == U256.fromU64(u64.MAX_VALUE - 1), 'substract ')
+    // 加法
+    // @ts-ignore
+    assert(U256.fromU64(123456789) + U256.fromU64(456789) == U256.fromU64(123913578), 'u256 add')
+
+    // 减法
+    // @ts-ignore    
+    assert((u - U256.one()) == U256.fromU64(u64.MAX_VALUE - 1), 'substract ')
+
+    // 乘法
+    // @ts-ignore
+    assert((U256.fromU64(123456789) * U256.fromU64(456789)) == U256.fromU64(56393703190521), 'u256 mul')
+
+    // 除法
+    // @ts-ignore
+    assert((U256.fromU64(123456789) / U256.fromU64(456789)) == U256.fromU64(270), 'u256 div')
     return Context.self()
 }
 
