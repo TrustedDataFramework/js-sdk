4eb90c61ec3564ad9a2b4315fde20f2340418908
zhuyingjie <m6567fc@outlook.com>
2020-11-24 17:07:10 +0800

    添加四则运算单元测试

--- test/u256.ts
+++ test/u256.ts
@@ -4,24 +4,39 @@ import { U256, log, Util, Context, Address } from '../lib'
 const u = U256.fromHex('ffffffffffffffff')
 
 export function init(): Address{
-    // 加法
-    // @ts-ignore
-    assert(U256.fromU64(123456789) + U256.fromU64(456789) == U256.fromU64(123913578), 'u256 add')
-
-    // 减法
-    // @ts-ignore    
-    assert((u - U256.one()) == U256.fromU64(u64.MAX_VALUE - 1), 'substract ')
-
-    // 乘法
-    // @ts-ignore
-    assert((U256.fromU64(123456789) * U256.fromU64(456789)) == U256.fromU64(56393703190521), 'u256 mul')
-
-    // 除法
-    // @ts-ignore
-    assert((U256.fromU64(123456789) / U256.fromU64(456789)) == U256.fromU64(270), 'u256 div')
     return Context.self()
 }
 
 export function getOne(): U256{
     return U256.one()
+}
+
+export function add(x: U256, y: U256): U256{
+    // @ts-ignore
+    return x + y
+}
+
+export function sub(x: U256, y: U256): U256{
+    // @ts-ignore
+    return x - y
+}
+
+export function mul(x: U256, y: U256): U256{
+    // @ts-ignore
+    return x * y
+}
+
+export function mulUnsafe(x: U256, y: U256): U256{
+    // @ts-ignore
+    return x.mul(y)
+}
+
+export function div(x: U256, y: U256): U256{
+    // @ts-ignore
+    return x / y
+}
+
+export function mod(x: U256, y: U256): U256{
+    // @ts-ignore
+    return x % y
 }
