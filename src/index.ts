e2d051e421d7e9749fd40e3d746a70f40bca35b1
zhuyingjie <m6567fc@outlook.com>
2021-01-06 17:16:13 +0800

    updates

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@ export { RPC, TransactionResult } from './rpc'
 export { TransactionBuilder } from './builder'
 export * as rlp from './rlp'
 export { Contract, compileContract, compileABI, getContractAddress, TypeDef, ABI } from './contract'
-export { constants, TX_STATUS } from './constants'
+export { constants, TX_STATUS, getEC, setEC, getHashAlgorithm, setHashAlgorithm } from './constants'
 export { privateKey2PublicKey, publicKey2Address, str2bin, hex2bin, bin2hex, bin2str, uuidv4, sign, generatePrivateKey, createKeyStore, randomBytes, KeyStore, readKeyStore, createAuthServer } from './utils'
 export { VirtualMachine } from './vm'
 import BN = require('../bn')
